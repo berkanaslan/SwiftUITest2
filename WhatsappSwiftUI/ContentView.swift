7f3ba847ff55013fa9414d85e1567c989b53e53d
Atil Samancioglu <samancioglu.atil@gmail.com>
2019-06-27 20:25:07 +0300

    Initial Commit

---
+++ WhatsappSwiftUI/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  WhatsappSwiftUI
+//
+//  Created by Atil Samancioglu on 27.06.2019.
+//  Copyright © 2019 Atil Samancioglu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
